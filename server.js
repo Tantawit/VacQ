be9ec0dd1923111eb27a293c5c35dc0a26ee4239
tantawit <tantawitp@gmail.com>
2023-02-09 15:57:32 +0700

    Do assignment 5: Rest + Mongo DB

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to db
+connectDB();
+
+//Route
+const hospitals = require("./routes/hospitals");
+
 const app = express();
 
-const hospitals = require("./routes/hospitals");
+//Body parser
+app.use(express.json());
+
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     "Server running in ",
@@ -18,3 +27,10 @@ app.listen(
     PORT
   )
 );
+
+//Handle promise
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
